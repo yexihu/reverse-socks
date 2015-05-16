7ebd3c9826527bd94ee51edf01e19dec665326bf
redpois0n <red@cmail.nu>
2015-05-16 13:17:48 +0200

    icon in constructor

--- Plugin Reverse Socks Client/src/se/jrat/plugin/socks/client/SocksPlugin.java
+++ Plugin Reverse Socks Client/src/se/jrat/plugin/socks/client/SocksPlugin.java
@@ -7,7 +7,7 @@ import jrat.api.ui.RATMenuItem;
 public class SocksPlugin extends Plugin {
 	
 	public SocksPlugin() {
-		super("Reverse SOCKS", "1.0", "Connect to internet through remote machine", "jRAT");
+		super("Reverse SOCKS", "1.0", "Connect to internet through remote machine", "jRAT", IconUtils.getIcon("icon", SocksPlugin.class));
 	
 		RATMenuItem entry = new RATMenuItem(new MenuListener(), "Reverse SOCKS", IconUtils.getIcon("icon", SocksPlugin.class));
 		RATMenuItem.addItem(entry);
