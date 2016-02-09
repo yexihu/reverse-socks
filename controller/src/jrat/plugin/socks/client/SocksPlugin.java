c5ba3eb7e20ccbd8b339d3e428ca65c3208903da
redpois0n <red@cmail.nu>
2016-02-09 10:32:16 +0100

    Fix icon

--- controller/src/jrat/plugin/socks/client/SocksPlugin.java
+++ controller/src/jrat/plugin/socks/client/SocksPlugin.java
@@ -1,14 +1,21 @@
 package jrat.plugin.socks.client;
 
-import jrat.api.Icons;
 import jrat.api.Plugin;
 import jrat.api.ui.RATMenuItem;
+
+import javax.swing.ImageIcon;
 
 public class SocksPlugin extends Plugin {
 	
 	public SocksPlugin() {
-		super("Reverse SOCKS", "1.2", "Connect to internet through remote machine", "jRAT", Icons.getIcon("Reverse SOCKS", "/icons/icon.png"));
-	
+		super("Reverse SOCKS", "1.2", "Connect to internet through remote machine", "jRAT");
+
+		try {
+			icon = new ImageIcon(getResource("icons/icon.png"));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		RATMenuItem entry = new RATMenuItem(new MenuListener(), "Reverse SOCKS", icon);
 		RATMenuItem.addItem(entry);
 	}
